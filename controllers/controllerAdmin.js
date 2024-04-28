bbf623a9faa312f3cd330e114eb14780e53e05c6
Bùi Bảo Lâm <buibaolam0302@gmail.com>
2024-04-28 15:38:49 +0700

    Binding data and finish layout header, footer

--- controllers/controllerAdmin.js
+++ controllers/controllerAdmin.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const mysql = require('mysql');
+const connection = require('../connectDB');
 
 const app = express();
 
 const getHandlerByClient = (req, res) => {
 };
-
-
 const postHandlerByClient = (req, res) => {};
 const putHandlerByClient = (req, res) => {};
 const deleteHandlerByClient = (req, res) => {};
 
-module.exports = {  getHandlerByClient, postHandlerByClient, putHandlerByClient , deleteHandlerByClient };
+module.exports = {  getHandlerByClient, postHandlerByClient, putHandlerByClient , deleteHandlerByClient};
