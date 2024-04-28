ef68c8428555a28b5221c640f9f982801a2c26c4
Bùi Bảo Lâm <114676429+TellYaHeadliner@users.noreply.github.com>
2024-04-28 15:44:07 +0700

    Add files via upload

--- NEWS.sql
+++ NEWS.sql
@@ -1,24 +1,62 @@
-CREATE DATABASE NEWS
-USE NEWS 
+CREATE DATABASE NEWS;
+USE NEWS; 
 
-CREATE TABLE LIENHE(
+CREATE TABLE footerLIENHE(
 	diaChiLienHe varchar (200),
 	email varchar (100),
 	facebook varchar (100),
 	youtube varchar (100),
-	thongTinBanQuyen (200)
+	thongTinBanQuyen varchar (200)
 )
 
 CREATE TABLE BAIVIET(
-	hinhAnhDaiDien blob,
+    maBaiViet int AUTO_INCREMENT PRIMARY KEY,
 	tieuDeBaiViet varchar (50),
-	ngayDang DEFAULT date(),
 	noiDungTomTat varchar (100),
-	maChuDe char (3)
+	noiDungBaiViet text,
+	hinhAnhDaiDien blob,
+	hinhAnhBaiViet blob,
+	ngayDang datetime DEFAULT CURRENT_TIMESTAMP(),
+	tacGia varchar (100),
+	maChuDe char (3),
+	trangThai bit
+)
+
+CREATE TABLE BINHLUAN(
+	maBaiViet int,
+	name varchar (100),
+	noiDung text
+)
+
+CREATE TABLE formLienHe(
+	hoTen varchar (200),
+	email char (100),
+	soDienThoai char (10),
+	tieuDe varchar (100),
+	ngayUpload datetime(),
+	status bit
 )
 
 CREATE TABLE CHUDE(
 	maChuDe char (3),
-	chuDe varchar (15)
+	chuDe varchar (15),
+	trangThai bit
 )
 
+CREATE TABLE TAIKHOAN(
+    email char (100),
+	tenTaiKhoan char (20),
+	matKhau char 256,
+	vaiTro int,
+	trangThai bit,
+)
+
+CREATE TABLE emailSubcribe(
+	email char (100) PRIMARY KEY,
+)
+
+INSERT INTO footerlienhe VALUES ('5 Đ. Huỳnh Thúc Kháng, Bến Nghé, Quận 1, Thành phố Hồ Chí Minh', 'abc@gmail.com', 'https://www.facebook.com/caothang.edu.vn', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', '0306221133-0306221136-0306221199')
+
+INSERT INTO news.chude
+(maChuDe, chuDe, trangThai)
+VALUES('KH', 'Khoa học', 1);
