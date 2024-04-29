9140f3424003aa8dce2fa647270c363c6ce53b7d
Bùi Bảo Lâm <buibaolam0302@gmail.com>
2024-04-29 15:04:16 +0700

    Change controllers folder to routes and changing routesGeneral

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
-const Client = require('./controllers/controllerClient');
-const Admin = require('./controllers/controllerAdmin');
-const General = require('./controllers/controllerGeneral')
+const Client = require('./routes/routesClients');
+const Admin = require('./routes/routesAdmin');
+const General = require('./routes/routesGeneral')
 const connnectDB = require('./connectDB');
 
 // GỌI PORT
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get('/', General.getMainByGeneral);
+app.use('/', General.routerGeneral);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at port ${process.env.PORT} `);
